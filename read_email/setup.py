f55bf0ea17ef2712f0da3a1828da5558d6da8070
Ali Alhamedi <ali.alhamdi@outlook.com>
2021-05-06 11:44:24 +0300

    First Commit

---
+++ read_email/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='read_email',
+      version='0.1',
+      description='reading local outlook inbox and save it',
+      author='Ali Alhamedi',
+      author_email='ali-alhamdi@outlook.com',
+      packages=['read_email'],
+)
